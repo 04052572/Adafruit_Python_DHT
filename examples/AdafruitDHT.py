9d171bdddda7212144924ca5884b91f0cdd7990b
04052572 <hank65116758a9u@gmail.com>
2017-12-11 12:00:29 +0000

    check point 3

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -19,8 +19,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 import time
 import sys
 import httplib, urllib
@@ -70,14 +72,20 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # If this happens try again!
 while True:
 	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
-	humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-	if humidity is not None and temperature is not None:
-		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-			{"dataChnId":"Temperature","values":{"value":t0}}]} 
-		post_to_mcs(payload)
-
+	SwitchStatus = GPIO.input(24)
+	if(SwitchStatus == 0):
+		print('Button pressed')
 	else:
-		print('Failed to get reading. Try again!')
-		sys.exit(1)
+		print('Button released')
+        if humidity is not None and temperature is not None:
+                print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature,humidity))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+					 {"dataChnId":"Temperature","values":{"value":t0}},
+					 {"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
+		post_to_mcs(payload)
+		time.sleep(1) 
+
+        else:
+                print('Failed to get reading. Try again!')
+                sys.exit(1)
 
